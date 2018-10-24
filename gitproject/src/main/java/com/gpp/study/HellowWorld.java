d919912d5c7cb271692231e96ae182a5ef8b6913
gaopingping <gaopingping@tuniu.com>
2018-10-24 21:08:47 +0800

    第二次项目提交

--- gitproject/src/main/java/com/gpp/study/HellowWorld.java
+++ gitproject/src/main/java/com/gpp/study/HellowWorld.java
@@ -4,10 +4,27 @@ package com.gpp.study;
  * Created by gaopingping on 2018/10/24.
  */
 public class HellowWorld {
-  public void eat()
-  {
+  /**
+   * eat
+   */
+  public void eat() {
     System.out.println("我的第一个git项目！");
   }
+
+  /**
+   *
+   * @param str
+   * @return str
+   */
+  public String getString(String str){
+      return str;
+    }
+
+
+
+
+
+
 
 
 }
